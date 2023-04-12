c1431ddea0525547aaa1e36d991ff2d530f090ed
yolandaw2021 <1621wyf@gmail.com>
2023-04-12 14:22:14 -0400

    Update environment.

---
+++ inference.py
@@ -0,0 +1,12 @@
+import torch
+from diffusers import DiffusionPipeline, DPMSolverMultistepScheduler
+from diffusers.utils import export_to_video
+
+pipe = DiffusionPipeline.from_pretrained("damo-vilab/text-to-video-ms-1.7b", torch_dtype=torch.float16, variant="fp16")
+pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
+pipe.enable_model_cpu_offload()
+
+prompt = "todo"
+video_frames = pipe(prompt, num_inference_steps=25).frames
+video_path = export_to_video(video_frames)
+print(video_path)

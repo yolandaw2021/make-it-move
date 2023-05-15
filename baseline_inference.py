475b6f5c138451e5004ccb29a2983a8a11c00888
yolandaw2021 <1621wyf@gmail.com>
2023-05-15 17:19:57 -0400

    Remove buggy code.

--- baseline_inference.py
+++ baseline_inference.py
@@ -5,26 +5,20 @@ from image_captioning import BLIP2
 from imageio import imread, imwrite
 
 def generate(image_path, pipeline, prompt, captioning, num_frames=16):
-    image = (torch.from_numpy(imread(image_path))/255.0).to(torch.float16)
-    print(image.shape)
-    w,h,c = image.shape
-    image = image[0:w//8*8, 0:h//8*8, :] #cropping to multiple of 8
-    if captioning:
-        prompt = utils.caption_image(image)
-    latents = torch.tile(image, (1, num_frames, 1, 1, 1))
-    latents = latents.permute(0, 2, 1, 3, 4)
-    print(type(latents))
-    print(latents.dtype)
+    # image = (torch.from_numpy(imread(image_path))/255.0).to(torch.float16)
+    # print(image.shape)
+    # w,h,c = image.shape
+    # image = image[0:w//8*8, 0:h//8*8, :] #cropping to multiple of 8
+    # w,h,c = image.shape
+    # if captioning:
+    #     prompt = utils.caption_image(image)
+    # latents = torch.tile(image, (1, num_frames, 1, 1, 1))
+    # latents = latents.permute(0, 4, 1, 2, 3)
 
-    video_frames = pipe(prompt, num_inference_steps=100, latents=latents, height=h, width=w, num_frames=num_frames).frames
-    video_path = export_to_video(video_frames, output_video_path='outputs/out.mp4')
+    # video_frames = pipe(prompt, num_inference_steps=100, latents=latents, height=h, width=w, num_frames=num_frames).frames
+    video_frames = pipe(prompt, num_inference_steps=100, num_frames=num_frames).frames
+    video_path = export_to_video(video_frames, output_video_path='outputs/meme0.mp4')
     print(video_path)
-
-# prompt = "A frog sitting under a pouring water pipe, looking very sad."
-# model = Blip2()
-# prompt = model.caption_image('frames/meme0.jpg')
-# latents = (batch_size, num_channel, num_frames, height, width)
-
 
 if __name__ == '__main__':
     pipe = DiffusionPipeline.from_pretrained("damo-vilab/text-to-video-ms-1.7b", torch_dtype=torch.float16, variant="fp16")

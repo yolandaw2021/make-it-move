0144bbb5df25605218d3187941e22c088c71980a
Fengyu Li <fengyuli2002@gmail.com>
2023-05-16 16:11:56 -0400

    Adjust prompts

--- chatgpt.py
+++ chatgpt.py
@@ -6,7 +6,8 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
 def query_gpt(vqa_prompts, vqa_responses, temperature=0):
-    prefix = "Describe a meme, as detailed and long as possible, given the following BLIP-generated response to multiple VQA prompts:\n"
+    prefix1 = "Describe a meme in detail, given the following BLIP-generated response to multiple VQA prompts:\n"
+    prefix2 = "Perform OCR on a meme, given the following BLIP-generated response to multiple VQA prompts:\n"
     vqa = "\n".join(
         [
             f"VQA prompt: {p}, VQA response: {r}"
@@ -14,9 +15,9 @@ def query_gpt(vqa_prompts, vqa_responses, temperature=0):
         ]
     )
     suffix1 = "\nNow, describe what the meme's about:"
-    suffix2 = "\nWhat is the text in the meme? Say No if there is no text:"
-    message1 = [{"role": "user", "content": prefix + vqa + suffix1}]
-    message2 = [{"role": "user", "content": prefix + vqa + suffix2}]
+    suffix2 = "\nWhat is the exact text in the meme? Say No if there is no text:"
+    message1 = [{"role": "user", "content": prefix1 + vqa + suffix1}]
+    message2 = [{"role": "user", "content": prefix2 + vqa + suffix2}]
     response1 = openai.ChatCompletion.create(
         model="gpt-4",
         messages=message1,

081cf302e7ff6dc526492e1d57f5bc1a5550c65d
yolandaw2021 <1621wyf@gmail.com>
2023-05-17 22:14:13 -0400

    Inference pipeline

--- baseline_inference.py
+++ baseline_inference.py
@@ -4,16 +4,17 @@ from diffusers.utils import export_to_video
 from image_captioning import BLIP2
 from imageio import imread, imwrite
 
-def generate(image_path, pipeline, prompt, captioning, num_frames=16):
+def generate(image_path, pipeline, prompt, num_frames=16, i):
     video_frames = pipe(prompt, num_inference_steps=100, num_frames=num_frames).frames
-    video_path = export_to_video(video_frames, output_video_path='outputs/meme0.mp4')
-    print(video_path)
+    video_path = export_to_video(video_frames, output_video_path='outputs/meme{i}.mp4')
+    print("Generated video to: meme", i)
 
 if __name__ == '__main__':
     pipe = DiffusionPipeline.from_pretrained("damo-vilab/text-to-video-ms-1.7b", torch_dtype=torch.float16, variant="fp16")
     pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
     pipe.enable_model_cpu_offload()
 
-    prompt = "Stick figure style. A sad and lonely person walking in the middle of a crowd, the crowd avoids the person whereever it moves. There is a radius of empty space around the person at all times."
-
-    generate('frames/meme0.jpg', pipe, prompt, captioning=False)
+    for i in tqdm(range(N), desc="Generating videos"):
+        with open(f'text_output/GPT/meme{i}.txt', 'r') as f:
+            prompt = f.read()
+        generate('frames/meme{i}.jpg', pipe, prompt, i)

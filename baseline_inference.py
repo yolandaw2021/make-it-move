3c29fff1aedff6f2bd34b3f8f017774f304af363
yolandaw2021 <1621wyf@gmail.com>
2023-05-18 12:58:35 -0400

    Inference with magic words.

--- baseline_inference.py
+++ baseline_inference.py
@@ -1,20 +1,19 @@
 import torch
 from diffusers import DiffusionPipeline, DPMSolverMultistepScheduler
 from diffusers.utils import export_to_video
-from image_captioning import BLIP2
 from imageio import imread, imwrite
-
-def generate(image_path, pipeline, prompt, num_frames=16, i):
-    video_frames = pipe(prompt, num_inference_steps=100, num_frames=num_frames).frames
-    video_path = export_to_video(video_frames, output_video_path='outputs/meme{i}.mp4')
-    print("Generated video to: meme", i)
+from tqdm import tqdm
 
 if __name__ == '__main__':
     pipe = DiffusionPipeline.from_pretrained("damo-vilab/text-to-video-ms-1.7b", torch_dtype=torch.float16, variant="fp16")
     pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
     pipe.enable_model_cpu_offload()
 
-    for i in tqdm(range(N), desc="Generating videos"):
-        with open(f'text_output/GPT/meme{i}.txt', 'r') as f:
-            prompt = f.read()
-        generate('frames/meme{i}.jpg', pipe, prompt, i)
+    for i in tqdm(range(50,219), desc="Generating videos"):
+        with open(f'text_output/GPT/meme{i}.txt', 'r') as gpt:
+            with open(f'text_output/OCR/meme{i}_ocr.txt', 'r') as ocr:
+                prompt = f"{gpt.read()}. Style: no words in the image; high-quality; generate best quality video. "
+                print(prompt)
+        video_frames = pipe(prompt, num_inference_steps=100, num_frames=16).frames
+        video_path = export_to_video(video_frames, output_video_path=f'outputs/meme{i}.mp4')
+        print("Generated video to: meme", i)
